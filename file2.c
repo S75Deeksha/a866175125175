f82dd12bc43b07bdfb68cb5fd172ab3c6076cb9a
S75Deeksha <SriDeeksha75Acharya@gmail.com>
2026-06-17 16:17:31 +0530

    added few lines of code in file2.cfile2.c

--- file2.c
+++ file2.c
@@ -4,5 +4,12 @@ int main() {
     int a = 5, b = 10;
     int sum = a + b;
     printf("Sum = %d\n", sum);
+    
+    int diff = a - b;
+    printf("Diff = %d\n", diff);
+    
+    int prod = a * b;
+    printf("Product = %d\n", prod);
+    
     return 0;
-}
+}}
